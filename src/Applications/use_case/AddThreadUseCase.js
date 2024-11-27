5def0a19224ccf46f717d46dee37dff5b7d5b1c8
Ali <akbarsatriaxtkj2@gmail.com>
2024-11-27 08:40:51 +0700

    feat: implement AddThreadUseCase with testing

---
+++ src/Applications/use_case/AddThreadUseCase.js
@@ -0,0 +1,14 @@
+const CreatedThread = require("../../Domains/threads/entities/CreateThread");
+
+class AddThreadUseCase {
+  constructor({ threadRepository }) {
+    this._threadRepository = threadRepository;
+  }
+
+  async execute(payload) {
+    const createThread = new CreatedThread(payload);
+    return this._threadRepository.addThread(createThread);
+  }
+}
+
+module.exports = AddThreadUseCase;
